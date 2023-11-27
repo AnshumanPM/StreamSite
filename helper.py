4a50d3ce49031d41824d3fc4bf5cd71ae0e0ce8d
Anshuman Prasad Mahanta <anshumanprasadmahanta@gmail.com>
2023-11-27 09:47:51 +0530

    Helper

---
+++ helper.py
@@ -0,0 +1,40 @@
+import json
+import os
+import re
+import urllib.parse
+from urllib.parse import unquote_plus
+
+import requests
+import validators
+from hashids import Hashids
+
+
+hash_salt = os.environ.get("HASH_SALT")
+hashids = Hashids(salt=hash_salt)
+
+
+def decode_string(encoded):
+    decoded = "".join([chr(i) for i in hashids.decode(encoded)])
+    return decoded
+
+
+def is_valid_url(url):
+    return validators.url(url)
+
+
+def auto_increment_id():
+    return int(collection.count_documents({})) + 1
+
+
+def extract_gdrive_id(gdrive_link):
+    if "drive.google.com" not in gdrive_link:
+        return None
+    match = re.match(
+        r"^https://drive\.google\.com/file/d/([a-zA-Z0-9_-]+)/?.*$", gdrive_link
+    )
+    if match:
+        return match.group(1)
+    query_params = urllib.parse.parse_qs(urllib.parse.urlparse(gdrive_link).query)
+    if "id" in query_params:
+        return query_params["id"][0]
+    return None
