7fdbb9af976975f3cb9c695329c59b890fb7e397
AnshumanPM <anshumanprasadmahanta@gmail.com>
2024-02-05 01:45:54 +0000

    Auto: Code Formatted
    Signed-off-by: AnshumanPM <anshumanprasadmahanta@gmail.com>

--- database.py
+++ database.py
@@ -3,10 +3,10 @@ from pymongo import MongoClient
 from config import DB_URL
 
 try:
-  client = MongoClient(DB_URL)
-  db = client["mydb"]
-  collection = db["links"]
-  new_collection = db["new_links"]
-except:
-  collection = None 
-  new_collection = None 
+    client = MongoClient(DB_URL)
+    db = client["mydb"]
+    collection = db["links"]
+    new_collection = db["new_links"]
+except BaseException:
+    collection = None
+    new_collection = None
