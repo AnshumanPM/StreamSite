a7a2c5030bcb58821bcca501b34da88414e1a154
Anshuman Prasad Mahanta <anshumanprasadmahanta@gmail.com>
2024-02-05 07:15:39 +0530

    Nothing

--- database.py
+++ database.py
@@ -2,7 +2,11 @@ from pymongo import MongoClient
 
 from config import DB_URL
 
-client = MongoClient(DB_URL)
-db = client["mydb"]
-collection = db["links"]
-new_collection = db["new_links"]
+try:
+  client = MongoClient(DB_URL)
+  db = client["mydb"]
+  collection = db["links"]
+  new_collection = db["new_links"]
+except:
+  collection = None 
+  new_collection = None 
